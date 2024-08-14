71151de477a572015877f4fb74838f67a5fd936d
Brian Kircher <36779728+bckircher@users.noreply.github.com>
2024-08-14 04:27:33 -0500

    Clean up the time keeper page.

--- src/main/resources/www/timekeeper/timekeeper.js
+++ src/main/resources/www/timekeeper/timekeeper.js
@@ -24,7 +24,7 @@ displayTime(time)
   sheet.setAttribute("id", "timekeeper_css");
 
   // Determine the color to use for the timer.
-  if((time <= 0) || ($(".button_reset").prop("disabled") == false))
+  if((time <= 0) || ($("#button_reset").prop("disabled") == false))
   {
     color = "var(--color-bright-red)";
   }
@@ -57,54 +57,113 @@ displayTime(time)
   document.body.appendChild(sheet);
 }
 
-// Opens the test screen, simplifying audio system testing.
-function
-openTest()
-{
-  // Show the test screen.
-  $(".test_bg").removeClass("hidden");
-  $(".test_main").removeClass("hidden");
-}
+// Tracks if the click handlers have been added to the test screen.
+var test_added = false;
 
-// Tests the start of match sound.
+// Shows the test screen, simplifying audio system testing.
 function
-testStart()
+showTest()
 {
-  // Send a message to the server to test the start of match sound.
-  ws.send("test start");
-}
+  // Get the dialog element.
+  const dialog = $("#test");
 
-// Tests the end game sound.
-function
-testEndGame()
-{
-  // Send a message to the server to test the end game sound.
-  ws.send("test end game");
-}
+  // Called when the test hide animation completes.
+  function
+  hideTestEnd()
+  {
+    // Remove the hide class (preparing the test screen for the next time it is
+    // displayed).
+    dialog.removeClass("hide");
 
-// Tests the end sound.
-function
-testEnd()
-{
-  // Send a message to the server to test the end sound.
-  ws.send("test end");
-}
+    // Close the modal dialog.
+    dialog.close();
 
-// Tests the cancel sound.
-function
-testCancel()
-{
-  // Send a message to the server ot test the cancel sound.
-  ws.send("test cancel");
-}
+    // Remove the end of animation event listener.
+    dialog.off("animationend", hideTestEnd);
 
-// Closes the test screen, returning to the timer start screen.
-function
-closeTest()
-{
-  // Hide the test screen.
-  $(".test_main").addClass("hidden");
-  $(".test_bg").addClass("hidden");
+    // Remove the keydown event listener.
+    $(document).off("keydown", showTest.keyDownTest);
+  }
+
+  // Hides the test screen.
+  function
+  hideTest()
+  {
+    // Add an end of animation event listener.
+    dialog.on("animationend", hideTestEnd);
+
+    // Add the hide class to the dialog, starting the close animation.
+    dialog.addClass("hide");
+  }
+
+  // Called when a key is pressed.
+  function
+  keyDownTest(e)
+  {
+    // Close the test screen if the escape key is pressed.
+    if(e.key == "Escape")
+    {
+      // Start the animated close of the test screen.
+      hideTest();
+
+      // Prevent any further handling of this keystroke.
+      e.preventDefault();
+    }
+  }
+
+  // Tests the start of match sound.
+  function
+  testStart()
+  {
+    // Send a message to the server to test the start of match sound.
+    ws.send("test start");
+  }
+
+  // Tests the end game sound.
+  function
+  testEndGame()
+  {
+    // Send a message to the server to test the end game sound.
+    ws.send("test end game");
+  }
+
+  // Tests the end sound.
+  function
+  testEnd()
+  {
+    // Send a message to the server to test the end sound.
+    ws.send("test end");
+  }
+
+  // Tests the cancel sound.
+  function
+  testCancel()
+  {
+    // Send a message to the server ot test the cancel sound.
+    ws.send("test cancel");
+  }
+
+  // See if the click handlers need to be added to the test screen.
+  if(!test_added)
+  {
+    // Add the click handlers to the test screen.
+    dialog.find(".test_start").on("click", testStart);
+    dialog.find(".test_end_game").on("click", testEndGame);
+    dialog.find(".test_end").on("click", testEnd);
+    dialog.find(".test_cancel").on("click", testCancel);
+    dialog.find(".test_exit").on("click", hideTest);
+
+    // Remmber that the click handlers have been added.
+    test_added = true;
+  }
+
+  // Show the test screen as a modal.
+  dialog.showModal();
+
+  // Add a keydown listener to the document to override the default Escape key
+  // handling for a modal dialog (which simply closes it, instead of animating
+  // the close like needed here).
+  $(document).on("keydown", keyDownTest);
 }
 
 // Starts the match.
@@ -153,27 +212,27 @@ wsMessage(e)
   if(e.data.substring(0, 7) === "m:run")
   {
     // Enable the timer cancel button.
-    $(".button_start").prop("disabled", true);
-    $(".button_cancel").prop("disabled", false);
-    $(".button_reset").prop("disabled", true);
+    $("#button_start").prop("disabled", true);
+    $("#button_cancel").prop("disabled", false);
+    $("#button_reset").prop("disabled", true);
   }
 
   // See if this is a mode stop message.
   else if(e.data.substring(0, 6) === "m:stop")
   {
     // Enable the timer reset button.
-    $(".button_start").prop("disabled", true);
-    $(".button_cancel").prop("disabled", true);
-    $(".button_reset").prop("disabled", false);
+    $("#button_start").prop("disabled", true);
+    $("#button_cancel").prop("disabled", true);
+    $("#button_reset").prop("disabled", false);
   }
 
   // See if this is a mode reset message.
   else if(e.data.substring(0, 7) === "m:reset")
   {
     // Enable the timer start button.
-    $(".button_start").prop("disabled", false);
-    $(".button_cancel").prop("disabled", true);
-    $(".button_reset").prop("disabled", true);
+    $("#button_start").prop("disabled", false);
+    $("#button_cancel").prop("disabled", true);
+    $("#button_reset").prop("disabled", true);
   }
 
   // See if this is a time message.
@@ -202,35 +261,27 @@ function
 ready()
 {
   // Set the function to call when the "menu" button is pressed.
-  $(".button_menu").click(showMenu);
+  $("#button_menu").click(showMenu);
 
   // Set the function to call when the "test" button is pressed.
-  $(".button_test").click(openTest);
+  $("#button_test").click(showTest);
 
   // Set the function to call when the "start" button is pressed.
-  $(".button_start").click(startMatch);
+  $("#button_start").click(startMatch);
 
   // Set the function to call when the "cancel" button is pressed.
-  $(".button_cancel").click(cancelMatch);
+  $("#button_cancel").click(cancelMatch);
 
   // Set the function to call when the "reset" button is pressed.
-  $(".button_reset").click(resetMatch);
-
-  // Set the functions to call when the audio test screen buttons are pressed.
-  $(".test_start").click(testStart);
-  $(".test_end_game").click(testEndGame);
-  $(".test_end").click(testEnd);
-  $(".test_cancel").click(testCancel);
-  $(".test_bg").click(closeTest);
-  $(".test_exit").click(closeTest);
+  $("#button_reset").click(resetMatch);
 
   // Display the not connected match length as the starting timer value.
   displayTime(-1);
 
   // Enable the start button and disable the cancel and reset buttons.
-  $(".button_start").prop("disabled", false);
-  $(".button_cancel").prop("disabled", true);
-  $(".button_reset").prop("disabled", true);
+  $("#button_start").prop("disabled", false);
+  $("#button_cancel").prop("disabled", true);
+  $("#button_reset").prop("disabled", true);
 
   // A key listener to enter/exit fullscreen mode when Ctrl+F is pressed.
   $(document).keypress(function(e)
@@ -252,19 +303,9 @@ ready()
   // A key listener to open the test menu with Ctrl+T is pressed.
   $(document).keypress(function(e)
   {
-    if((e.keyCode == 20) && ($(".test_main").hasClass("hidden") == true))
+    if((e.keyCode == 20) && ($("#test").is(":visible") == false))
     {
-      openTest();
-      return(false);
-    }
-  });
-
-  // A key listener to close the test menu when the escape key is pressed.
-  $(document).keydown(function(e)
-  {
-    if((e.keyCode == 27) && ($(".test_main").hasClass("hidden") == false))
-    {
-      closeTest();
+      showTest();
       return(false);
     }
   });
