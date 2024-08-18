9c5d7d301064dcd7c8076036b716afd672225501
Brian Kircher <36779728+bckircher@users.noreply.github.com>
2024-08-18 08:34:52 -0500

    Suppress the SLF4J message at startup.

--- src/main/java/FLLScorer/Main.java
+++ src/main/java/FLLScorer/Main.java
@@ -10,6 +10,7 @@ public class Main
   public static void
   main(String[] args)
   {
+    System.setProperty("slf4j.internal.verbosity", "ERROR");
     Database.getInstance().setup();
     Config.getInstance().setup();
     WebServer.getInstance().setup();
